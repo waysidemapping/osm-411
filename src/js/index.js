62f484838ad0edc04cc16e3ce73bcd16254e4dc2
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2024-12-07 16:14:10 -0500

    Simplify coordinates for example

--- src/js/index.js
+++ src/js/index.js
@@ -207,7 +207,7 @@ function reloadPage() {
   if (params.z && params.lat && params.lon) {
     descHtml += `<p>Pages will open at latitude <code>${params.lat}</code>, longitude <code>${params.lon}</code>, and zoom <code>${params.z}</code>. <a href="#">Clear</a></p>`;
   } else {
-    descHtml += `<p>You can set a common viewport with the URL hash like <code>#map=zoom/lat/lon</code>. <a href="#map=14/39.952399/-75.163613">Example</a></p>`;
+    descHtml += `<p>You can set a common viewport with the URL hash like <code>#map=zoom/lat/lon</code>. <a href="#map=14/39.9524/-75.1636">Example</a></p>`;
   }
   document.getElementById('header-desc').innerHTML = descHtml;
 
